58acb880dc6acfc82cea2b6131ed8247e45590a6
Hanno Schlichting <hanno@hannosch.eu>
2006-10-01 09:53:25 +0000

    Adjusted actions installation tracking to work with CMF 2.1+.
    svn path=/CMFQuickInstallerTool/trunk/; revision=31251

--- Products/CMFQuickInstallerTool/InstalledProduct.py
+++ Products/CMFQuickInstallerTool/InstalledProduct.py
@@ -291,8 +291,13 @@ class InstalledProduct(SimpleItem):
             portal_actions=getToolByName(self,'portal_actions')
             if CMF21:
                 for category, action in self.actions:
-                    # XXX
-                    pass
+                    if category in portal_actions.objectIds():
+                        cat = portal_actions[category]
+                        if action in cat.objectIds():
+                            cat._delObject(action)
+                        if len(cat.objectIds()) == 0:
+                            del cat
+                            portal_actions._delObject(category)
             else:
                 actids= [o.id.lower() for o in portal_actions._actions]
                 delactions=[actids.index(id) for id in

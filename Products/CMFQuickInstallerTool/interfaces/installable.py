5fe40d00d55a0b0bfe3dc48898f37055e3795205
Vincent Fretin <vincent.fretin@gmail.com>
2009-05-24 18:54:25 +0000

    Reverted r86648. From Wichert: This looks like it will break all profiles which are registered for ISiteRoot or IPloneSite, which is a very common convention
    svn path=/CMFQuickInstallerTool/branches/2.1/; revision=86669

--- Products/CMFQuickInstallerTool/interfaces/installable.py
+++ Products/CMFQuickInstallerTool/interfaces/installable.py
@@ -6,9 +6,3 @@ class INonInstallable(Interface):
         """Returns a list of products that should not be available for
            installation.
         """
-
-class IInstallable(Interface):
-    """Used by QuickInstallerTool to only list extension profiles registered for IInstallable
-       interface or not specified ('for' attribute not specified in the registerProfile zcml directive).
-       This fixes extension profiles used with an upgradeStep showing up in the list returned by listProfileInfo.
-    """
